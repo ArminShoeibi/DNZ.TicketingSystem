cf2e61440a1a53144f23fae87e06d49ae473a25a
Armin Shoeibi <68565441+ArminShoeibi@users.noreply.github.com>
2021-02-09 22:41:41 +0330

    Add project files.

---
+++ DNZ.TicketingSystem.GrpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DNZ.TicketingSystem.GrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DNZ.TicketingSystem.GrpcServer/Enumerations/TicketStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace DNZ.TicketingSystem.GrpcServer.Enumerations
+{
+    public enum TicketStatus
+    {
+        New = 0,
+        Open = 1,
+        InProgress = 2,
+        Closed = 3,
+        Deleted = 4,
+        Canceled = 4,
+    }
+}
